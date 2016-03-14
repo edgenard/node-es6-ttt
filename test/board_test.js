077c2cb1ddc87f92c58d0011ac90100f81cff066
Emmanuel Genard <edgenard@gmail.com>
2016-03-14 14:40:42 -0400

    board has default state

--- test/board_test.js
+++ test/board_test.js
@@ -4,7 +4,12 @@ import {Board} from "../lib/board.js"
 
 describe("Board", function () {
   it("board state is an array", function () {
-    const board = Board();
+    const board = Board()
     assert.isArray(board.state)
+  })
+
+  it("board.state defaults to numbers 0 to 8", function () {
+    const board = Board()
+    assert.deepEqual(board.state, [0, 1, 2, 3, 4, 5, 6, 7, 8])
   })
 })

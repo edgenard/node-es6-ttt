de479ce2a32b39a596dc73a1e0a86b9e62254b32
Emmanuel Genard <edgenard@gmail.com>
2016-03-14 15:31:04 -0400

    placeMark returns board unchanged if position is already filled

--- test/board_test.js
+++ test/board_test.js
@@ -1,6 +1,6 @@
 
 import {assert} from "chai"
-import {board} from "../lib/board.js"
+import {board, placeMark} from "../lib/board.js"
 
 describe("Board", function () {
   it("board is an array", function () {
@@ -12,5 +12,18 @@ describe("Board", function () {
     assert.deepEqual(board(), [0, 1, 2, 3, 4, 5, 6, 7, 8])
   })
 
+  it("placeMark puts mark and produces a new board",function () {
+    const newBoard = placeMark({currentBoard: board(), position: 4, mark: "X"} )
+    assert.deepEqual(newBoard, [0, 1, 2, 3, "X", 5, 6, 7, 8])
+  })
+
+  it("placeMark returns board unchanged if spot is filled", function () {
+    const newBoard = placeMark({
+      currentBoard: board([0, 1, 2, 3, "X", 5, 6, 7, 8]),
+      position: 4,
+      mark: "O"
+    })
+    assert.deepEqual(newBoard, [0, 1, 2, 3, "X", 5, 6, 7, 8] )
+  })
 
 })

7be82c3e9c5e948b6f0fcb157cf6cd84b5f89ccd
Emmanuel Genard <edgenard@gmail.com>
2016-03-14 14:51:01 -0400

    changed board to just be a simple array

--- test/board_test.js
+++ test/board_test.js
@@ -3,13 +3,14 @@ import {assert} from "chai"
 import {Board} from "../lib/board.js"
 
 describe("Board", function () {
-  it("board state is an array", function () {
-    const board = Board()
-    assert.isArray(board.state)
+  it("board is an array", function () {
+    const board = Board([])
+    assert.isArray(board)
   })
 
-  it("board.state defaults to numbers 0 to 8", function () {
+  it("board  defaults to numbers 0 to 8", function () {
     const board = Board()
-    assert.deepEqual(board.state, [0, 1, 2, 3, 4, 5, 6, 7, 8])
+    assert.deepEqual(board, [0, 1, 2, 3, 4, 5, 6, 7, 8])
   })
+
 })

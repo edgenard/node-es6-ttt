828c8e909b8cd4d1ca05c45b6c1787aec2b08567
Emmanuel Genard <edgenard@gmail.com>
2016-03-14 14:54:31 -0400

    Changed board to be a function that just returns an array

--- test/board_test.js
+++ test/board_test.js
@@ -1,16 +1,16 @@
 
 import {assert} from "chai"
-import {Board} from "../lib/board.js"
+import {board} from "../lib/board.js"
 
 describe("Board", function () {
   it("board is an array", function () {
-    const board = Board([])
-    assert.isArray(board)
+
+    assert.isArray(board())
   })
 
-  it("board  defaults to numbers 0 to 8", function () {
-    const board = Board()
-    assert.deepEqual(board, [0, 1, 2, 3, 4, 5, 6, 7, 8])
+  it("board defaults to numbers 0 to 8", function () {
+    assert.deepEqual(board(), [0, 1, 2, 3, 4, 5, 6, 7, 8])
   })
+
 
 })

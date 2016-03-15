85ea1888165e0efa88db3131512cfbb6f0f3de06
Emmanuel Genard <edgenard@gmail.com>
2016-03-15 14:33:16 -0400

    board is now 2d array, board function returns object with properties that can be called, other functions have been updated to reflect new type of board

--- test/board_test.js
+++ test/board_test.js
@@ -3,49 +3,49 @@ import {assert} from "chai"
 import {board, placeMark, checkRows} from "../lib/board.js"
 
 describe("Board", function () {
-  it("board is an array", function () {
+  it("board returns an object", function () {
 
-    assert.isArray(board())
+  assert.isObject(board())
   })
 
   it("board defaults to numbers 0 to 8", function () {
-    assert.deepEqual(board(), [0, 1, 2, 3, 4, 5, 6, 7, 8])
+    assert.deepEqual(board().state, [[0, 1, 2], [3, 4, 5], [6, 7, 8]])
   })
 
   it("placeMark puts mark and produces a new board",function () {
     const newBoard = placeMark({currentBoard: board(), position: 4, mark: "X"} )
-    assert.deepEqual(newBoard, [0, 1, 2, 3, "X", 5, 6, 7, 8])
+    assert.deepEqual(newBoard.state, [[0, 1, 2], [3, "X", 5], [6, 7, 8]])
   })
 
   it("placeMark returns board unchanged if spot is filled", function () {
     const newBoard = placeMark({
-      currentBoard: board([0, 1, 2, 3, "X", 5, 6, 7, 8]),
+      currentBoard: board([[0, 1, 2], [3, "X", 5], [6, 7, 8]]),
       position: 4,
       mark: "O"
     })
-    assert.deepEqual(newBoard, [0, 1, 2, 3, "X", 5, 6, 7, 8] )
+    assert.deepEqual(newBoard.state, [[0, 1, 2], [3, "X", 5], [6, 7, 8]] )
   })
 
   it("checkRows returns the winning mark for top row", function () {
-    const gameBoard = board(["X", "X", "X", 3, 4, 5, 6, 7, 8])
+    const gameBoard = board([["X", "X", "X"], [3, 4, 5], [6, 7, 8]])
 
     assert.equal(checkRows(gameBoard), "X")
   })
 
   it("checkRows returns winning mark for middle row", function () {
-    const gameBoard = board([0, 1, 2, "O", "O", "O", 6, 7, 8])
+    const gameBoard = board([[0, 1, 2], ["O", "O", "O"], [6, 7, 8]])
 
     assert.equal(checkRows(gameBoard), "O")
   })
 
   it("checkRows returns winning mark for bottom row", function () {
-    const gameBoard = board([0, 1, 2, 3, 4, 5, "X", "X", "X"])
+    const gameBoard = board([[0, 1, 2], [3, 4, 5], ["X", "X", "X"]])
 
     assert.equal(checkRows(gameBoard), "X")
   })
 
   it("checkRows returns false if no winner", function () {
-    const gameBoard = board([0, 1, "O", "O", "O", 5, 6, "X", "X"])
+    const gameBoard = board([[0, 1, "O"], ["O", "O", 5], [6, "X", "X"]])
 
     assert.equal(checkRows(gameBoard), false)
   })
